78068b1565d9d4bd3c847ece6496c82c4bd8da07
alok <alokbarmanraj2021@gmail.com>
2026-04-30 17:29:26 +0600

    navbar responsive

--- src/components/NavSearch.jsx
+++ src/components/NavSearch.jsx
@@ -1,13 +1,34 @@
 "use client";
-import { Label, SearchField } from "@heroui/react";
+import { SearchField } from "@heroui/react";
 
 const NavSearch = () => {
   return (
-    <div className="w-full px-4">
-      <div className="w-full sm:w-[400px] md:w-[500px] lg:w-[720px]">
+    <div className="w-full flex justify-center px-2">
+      <div
+        className="
+          w-full 
+          max-w-[150px] 
+          sm:max-w-[200px] 
+          md:max-w-[240px] 
+          lg:max-w-[720px] 
+        "
+      >
         <SearchField name="search">
-          <SearchField.Group className="flex items-center gap-2 border rounded-md px-3 py-2 cursor-pointer">
-            <SearchField.Input className="flex-1 outline-none" />
+          <SearchField.Group
+            className="
+              flex items-center gap-2 
+              border rounded-md 
+              px-2 sm:px-3 
+              py-1.5 sm:py-2
+              bg-white
+              w-full
+              overflow-hidden
+            "
+          >
+            <SearchField.Input
+              className="flex-1 outline-none text-sm min-w-0"
+              placeholder="Search..."
+            />
             <SearchField.ClearButton />
             <SearchField.SearchIcon />
           </SearchField.Group>
